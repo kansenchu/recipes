9122fe7d18bee7b136e34234e8ba0097c28645c0
Wilson Choo <kansen.chu@g.softbank.co.jp>
2019-07-07 23:41:53 +0900

    change properties

--- src/main/java/co/softbank/recieverecipe/controller/BasicRecipeController.java
+++ src/main/java/co/softbank/recieverecipe/controller/BasicRecipeController.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
