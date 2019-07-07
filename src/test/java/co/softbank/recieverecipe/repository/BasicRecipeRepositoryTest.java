127b389d84419f0155725d6c7df2ad97ccd607cc
Wilson Choo <kansen.chu@g.softbank.co.jp>
2019-07-07 09:43:37 +0900

    getRecipe from repo failing test

--- src/test/java/co/softbank/recieverecipe/repository/BasicRecipeRepositoryTest.java
+++ src/test/java/co/softbank/recieverecipe/repository/BasicRecipeRepositoryTest.java
@@ -1,11 +1,14 @@
 package co.softbank.recieverecipe.repository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import co.softbank.recieverecipe.TestObjectRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,7 +17,7 @@ public class BasicRecipeRepositoryTest {
   BasicRecipeRepository basicRecipeRepo;
 
   @Test
-  public void initializationTest() {
-    assertTrue(true);
+  public void getRecipe() {
+    assertEquals(basicRecipeRepo.getRecipe(1), TestObjectRepo.recipeOne);
   }
 }
